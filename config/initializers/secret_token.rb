b86cfe98a1cb5283af529ef0a04575f2c082b303
nadine <neauro@gmail.com>
2013-09-02 13:38:41 -0700

    rails wooo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VcdPlay::Application.config.secret_token = '86bb6be8afde67ae992ff2bb29f06ac36e8b850253a1c0f415e00e0ec760cd78e27b7f59cf4bfeb68cd45698ca0898874b9454d65c3829cbdb58e302238141ee'
